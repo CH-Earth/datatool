4bf21b6a10af8a5b4146eb98c344b9c109811440
Kasra Farmer <kasra.keshavarz@usask.ca>
2023-05-04 15:32:01 -0400

    Exiting the script of no overlaps happens on domains of the dataset

--- daymet/daymet.sh
+++ daymet/daymet.sh
@@ -352,7 +352,7 @@ wait
 # finalizing the workflow
 mkdir "$HOME/empty_dir"
 echo "$(basename $0): deleting temporary files from $cache"
-#rsync -aP --delete "$HOME/empty_dir/" "$cache"
+rsync -aP --delete "$HOME/empty_dir/" "$cache"
 rm -r "$cache"
 echo "$(basename $0): temporary files from $cache are removed"
 echo "$(basename $0): results are produced under $outputDir"
